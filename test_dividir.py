00208a939e9e7be6be1e6bc1a4718d864e4ced2b
mariloli02 <mariloli6326@gmail.com>
2023-11-23 21:08:03 +0100

    Añadida division por cero

--- test_dividir.py
+++ test_dividir.py
@@ -5,6 +5,8 @@ class TestDividir(unittest.TestCase):
     def test_dividir(self):
         self.assertEqual(division(10, 5), 2)
         self.assertEqual(division(-1, 1), -1)
+        with self.assertRaises(ZeroDivisionError):
+            resultado = division(1, 0)
         self.assertEqual(division(-1, -1), 1)
 
 
